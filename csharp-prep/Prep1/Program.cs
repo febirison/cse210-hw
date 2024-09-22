6df80de3b8b739af65fc9d147b4b6dbdd4df4e69
febirison <andersonokai@gmail.com>
2024-09-22 16:29:22 -0500

    Completed C# Prep 1 assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,20 @@
+// program by Anderson OKai
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Prompt for first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Prompt for last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Output the formatted name
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
